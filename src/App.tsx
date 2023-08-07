5ea4e241d8575d7ad70e3ef5d212b957d814ea41
Natan <mouranatan933@gmail.com>
2023-08-07 10:09:10 -0300

    feat: screen login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Login from "./components/Login";
 import Ways from "./routes";
 
 const App = () => {
   return (
     <main className="bg-emerald-300">
       <Ways />
+      <Login/>
     </main>
   );
 };
